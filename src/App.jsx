c4bb86620eeddc6f326c4d2c950fca044684d9d4
kvijay1710 <Vijay@Pixalive.me>
2025-05-28 17:23:55 +0530

    top scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import Top from "./components/Top";
 
 function App() {
   return (
     <>
       <div>
+        <Top />
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
